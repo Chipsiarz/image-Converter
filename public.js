7004dd0bd18aa3c0825b848cb35db46f778b80b5
Chipsu <kamil123456789-20@o2.pl>
2022-12-05 20:26:13 +0100

    fix

--- public.js
+++ public.js
@@ -2,13 +2,9 @@ const inputJpeg = document.getElementById("jpegInput");
 const inputPng = document.getElementById("pngInput");
 
 inputJpeg.addEventListener("change", () => {
-  console.log("działa1");
   inputPng.value = "";
 });
 
 inputPng.addEventListener("change", () => {
-  console.log("działą2");
   inputJpeg.value = "";
 });
-
-console.log("działa3");
